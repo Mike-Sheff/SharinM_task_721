cd43f886a6c945990b13e3422c11b0ef1e01293d
Mike-Sh <mike-sheff@yandex.ru>
2019-07-09 16:20:15 +0300

    update click checkbox

--- app/src/main/java/ru/netologia/sharinm_task_721/MainActivity.java
+++ app/src/main/java/ru/netologia/sharinm_task_721/MainActivity.java
@@ -4,36 +4,54 @@ import android.content.Intent;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Layout;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    private LinearLayout layoutplace;
+    private LinearLayout layoutLatitude;
+    private LinearLayout layoutLongitude;
+    private CheckBox checkBox;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final EditText textSearch =findViewById(R.id.editText);
-        final EditText textLatitude =findViewById(R.id.Latitude);
-        final EditText textLongitude =findViewById(R.id.Longitude);
+        final EditText textSearch = findViewById(R.id.editText);
+        final EditText textLatitude = findViewById(R.id.Latitude);
+        final EditText textLongitude = findViewById(R.id.Longitude);
+        layoutplace = findViewById(R.id.linearPlace);
+        layoutLatitude = findViewById(R.id.linearCoordinatesLatitude);
+        layoutLongitude = findViewById(R.id.linearCoordinatesLongitude);
         Button btnSearch = findViewById(R.id.buttonSearch);
-        final CheckBox checkBox = findViewById(R.id.checkbox);
+        checkBox = findViewById(R.id.checkbox);
 
-
-        btnSearch.setOnClickListener(new View.OnClickListener() {
+        btnSearch.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(Intent.ACTION_VIEW);
                 String search;
 
                 if (checkBox.isChecked()) {
-                    search = "geo:?q=" + textSearch.getText().toString();
+                    if (textSearch.getText().toString().isEmpty()){
+                     search = getString(R.string.textSearchPlace, textSearch.getHint().toString());
+                    } else {
+                        search =  getString(R.string.textSearchPlace, textSearch.getText().toString());
+                    }
                 } else {
-
-                    search = "geo:" + textLatitude.getText().toString() + ", " + textLongitude.getText().toString();
+                    if (textSearch.getText().toString().isEmpty()){
+                        search = getString(R.string.textSearchCoordinates, textLatitude.getHint().toString(), textLongitude.getHint().toString());
+                    } else {
+                        search = getString(R.string.textSearchCoordinates, textLatitude.getText().toString(), textLongitude.getText().toString());
+                    }
                 }
 
                 Uri uri = Uri.parse(search);
@@ -44,5 +62,22 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        CompoundButton.OnCheckedChangeListener checkedChangeListener = new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+
+                if (checkBox.isChecked()) {
+                    layoutplace.setVisibility(View.VISIBLE);
+                    layoutLatitude.setVisibility(View.GONE);
+                    layoutLongitude.setVisibility(View.GONE);
+                } else {
+                    layoutplace.setVisibility(View.GONE);
+                    layoutLatitude.setVisibility(View.VISIBLE);
+                    layoutLongitude.setVisibility(View.VISIBLE);
+                }
+            }
+        };
+
+        checkBox.setOnCheckedChangeListener(checkedChangeListener);
     }
 }
